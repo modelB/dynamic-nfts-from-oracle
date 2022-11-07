8d48c538d37cbb82a71dc7944a8f5613ca3a3536
Remix Workspace <>
2022-11-06 22:03:58 -0500

    updates

--- contracts/Bull&Bear.sol
+++ contracts/Bull&Bear.sol
@@ -14,20 +14,53 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // This import includes functions from both ./KeeperBase.sol and
 // ./interfaces/KeeperCompatibleInterface.sol
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
 // Dev imports. This only works on a local dev network
 // and will not work on any test or main livenets.
 import "hardhat/console.sol";
 
-contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, KeeperCompatibleInterface {
+contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, KeeperCompatibleInterface, VRFConsumerBaseV2 {
     using Counters for Counters.Counter;
     AggregatorV3Interface public pricefeed;
-
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
     Counters.Counter private _tokenIdCounter;
     uint256 lastUpdated = block.timestamp;
     uint256 public /* immutable */ interval;
     int256 public currentPrice;
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+    VRFCoordinatorV2Interface COORDINATOR;
 
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    uint256[] public s_randomWords;
+
+    bytes32 keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 100000;
+
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
+    uint32 numWords = 1;
+
+    enum MarketTrend{BULL, BEAR} // Create Enum
+    MarketTrend public currentMarketTrend = MarketTrend.BULL; 
+
+    address private immutable vrfCoordinator;
   
 
     // IPFS URIs for the dynamic nft graphics/metadata.
@@ -46,7 +79,11 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Keeper
 
     event TokensUpdated(string marketTrend);
 
-    constructor(uint256 intervalArg, address _pricefeed) ERC721("Bull&Bear", "BBTK") {
+    // goerli vrfcoordinator 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D
+    constructor(uint256 intervalArg, address _pricefeed, uint64 subscriptionId, address _vrfCoordinator) ERC721("Bull&Bear", "BBTK") {
+        s_subscriptionId = subscriptionId;
+        vrfCoordinator = _vrfCoordinator;
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
         interval = intervalArg;
 
         pricefeed = AggregatorV3Interface(_pricefeed); // To pass in the mock
@@ -74,6 +111,50 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Keeper
             " and assigned token url: ",
             defaultUri
         );
+    }
+
+    function requestRandomWords() external onlyOwner returns (uint256 requestId) {
+        // Will revert if subscription is not set and funded.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({randomWords: new uint256[](0), exists: true, fulfilled: false});
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
+        // randomWords looks like this uint256: 68187645017388103597074813724954069904348581739269924188458647203960383435815
+
+        console.log("...Fulfilling random Words");
+        
+        string[] memory urisForTrend = currentMarketTrend == MarketTrend.BULL ? bullUrisIpfs : bearUrisIpfs;
+        uint256 idx = randomWords[0] % urisForTrend.length; // use modulo to choose a random index.
+
+
+        for (uint i = 0; i < _tokenIdCounter.current() ; i++) {
+            _setTokenURI(i, urisForTrend[idx]);
+        } 
+
+        string memory trend = currentMarketTrend == MarketTrend.BULL ? "bullish" : "bearish";
+        
+        emit TokensUpdated(trend);
+    }
+
+    function getRequestStatus(uint256 _requestId) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, 'request not found');
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
     }
 
     // The following functions are overrides required by Solidity.
@@ -190,5 +271,19 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Keeper
 
     function compareStrings(string memory a, string memory b) internal pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    // For VRF Subscription Manager
+    function setSubscriptionId(uint64 _id) public onlyOwner {
+        s_subscriptionId = _id;
+    }
+
+
+    function setCallbackGasLimit(uint32 maxGas) public onlyOwner {
+        callbackGasLimit = maxGas;
+    }
+
+    function setVrfCoordinator(address _address) public onlyOwner {
+        COORDINATOR = VRFCoordinatorV2Interface(_address);
     }
 }
